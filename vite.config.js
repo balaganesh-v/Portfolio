4e6d2b8e9f579bd901104ba298f421f00eed640a
balaganesh-v <vbalaganesh2007@gmail.com>
2025-12-09 15:51:30 +0530

    Update the Vite file to run on multiple hosts like ngrok.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,       // allows network access
+    strictPort: false,
+    allowedHosts: 'all'  // ✅ must be literal string 'all'
+  }
 })
